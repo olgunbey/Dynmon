d492aebd4e85406d96c2a5437a33243926517391
olgunbey <olgunsahin0161@hotmail.com>
2025-04-21 15:30:32 +0300

    feat: Added overload for ExecuteQueryAsync to accept connection parameters

---
+++ Dynmon/MongoQueryRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace Dynmon
+{
+    public class MongoQueryRequest
+    {
+        public IEnumerable<Filter> Filters { get; set; }
+        public List<Lookup> Lookups { get; set; }
+        public List<string> SelectedFields { get; set; }
+    }
+}
